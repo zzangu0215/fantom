814bf6c51ada16781db5e3b81ae56e69d0139a02
wooglow <wooglow@gmail.com>
2021-06-29 10:30:34 -0700

    First build up

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
-script.js
+var baseUrl = "https://gateway.marvel.com";
+var publicApi = "5676e7d9c3a3777b9fb6a77f56ea448c";
+// var searchword = $('#search-hero');
+
+var searchword = "Iron man";
+
+function searchHero(searchword) {
+    if (searchword) {
+        queryUrl = "https://gateway.marvel.com?"
+    }
+
+}
