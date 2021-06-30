947266b032da1abe16a7a22f1d50508baea68088
Israel Magallon <browncans@gmail.com>
2021-06-29 17:56:00 -0700

    made block for hearted heroes and add to hearted function

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?";
 var publicApi = "ca005abda60b0404cf146a9cb51c1cce40fb094d";
+var heartContainer = document.querySelector("#hearcontianer");
 // var searchword = $('#search-hero');
 
 var searchword = "Iron man";
@@ -25,3 +26,15 @@ function searchHero(searchword) {
 
 searchHero();
 
+
+
+function addToHeartedList(){
+    var heartedHero = document.createElement('<a>');
+
+    heartedHero.classList.add("button expanded", "btn");
+
+    heartedHero.setAttribute("data-hero", searchword);
+    heartedHero.textContent = searchword;
+    heartContainer.appendChild(searchword)
+
+}
