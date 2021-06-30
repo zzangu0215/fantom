26c9348d382bc2118285998ea9ed83b88ff224e0
Junny <tajo0215@gmail.com>
2021-06-30 12:04:57 -0700

    Submit Button for showing the hero cards successfully done, Fetching Youtube Data done

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,60 @@
 var baseUrl = "https://gateway.marvel.com:443/v1/public/characters?";
 var heartContainer = document.querySelector("#hearcontianer");
 var publicApi = "5676e7d9c3a3777b9fb6a77f56ea448c";
-var searchword = "Iron man";
+//var searchword = "Iron man";
 
 //var submitButton = document.querySelector("#submit-button");
 //var heroInput = document.querySelector("#hero-input");
+var heroActualNames = [
+    {
+        heroname: "Iron man",
+        realname: "Tony Stark"
+    },
+    {
+        heroname: "Captain America",
+        realname: "Steve Rodgers"
+    },
+    {
+        heroname: "Spider-man",
+        realname: "Tony Stark"
+    },
+    {
+        heroname: "Thor",
+        realname: "Thor Odinson"
+    },
+    {
+        heroname: "Ant-man",
+        realname: "Scott Lang"
+    },
+    {
+        heroname: "Black Widow",
+        realname: "Natasha Romanoff"
+    },
+    {
+        heroname: "Hawkeye",
+        realname: "Clint Barton"
+    },
+    {
+        heroname: "Hulk",
+        realname: "Bruce Banner"
+    },
+    {
+        heroname: "Captain Marvel",
+        realname: "Carol Danvers"
+    },
+    {
+        heroname: "Black Panther",
+        realname: "T'Challa"
+    },
+]
+
 
 function searchHero(searchword) {
     if (searchword) {
-        console.log(searchword);
+        //console.log(searchword);
         baseUrl = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchword + "&apikey=5676e7d9c3a3777b9fb6a77f56ea448c";
-    }console.log(baseUrl);
+    }
+    //console.log(baseUrl);
     fetch(baseUrl)
     .then(function(response){
         if (!response.ok) {
@@ -19,27 +63,60 @@ function searchHero(searchword) {
             return response.json();
             })
     .then(function(data){
-        // showing data
-        console.log(data);
-        console.log(data.data.results);
-        console.log(data.data.results[0].id);
-        console.log(data.data.results[0].name);
-        console.log(data.data.results[0].description);
-        //console.log(data.data.results[0].thumbnail[0].path);
-        //console.log(data.data.results[0].thumbnail[0].extension);
-
-        var heroid = data.data.results[0].id;
-        var heroname = data.data.results[0].name;
-        var heroDescription = data.data.results[0].description;
-        var heroThumbnail = data.data.results[0].thumbnail[0].path + data.data.results[0].thumbnail[0].extension;
         
-
-        
-        
+        heroPage(data);        
+           
         // 
     })
 
+}
+
+function heroPage(data) {
+    // showing data
+    console.log(data);
+    console.log(data.data.results);
+    console.log(data.data.results[0].id);
+    console.log(data.data.results[0].name);
+    console.log(data.data.results[0].description);
+    console.log(data.data.results[0].thumbnail.path + "/standard_fantastic.jpg");
+    console.log(data.data.results[0].urls[2].type)
+    console.log(data.data.results[0].urls[2].url);
+    //console.log(data.data.results[0].thumbnail[0].path);
+    //console.log(data.data.results[0].thumbnail[0].extension);
+
+    //var heroid = data.data.results[0].id;
+    var heroname = data.data.results[0].name;
+    var heroDescription = data.data.results[0].description;
+    var thumbnail = data.data.results[0].thumbnail.path + "/standard_fantastic.jpg";
+    //var heroThumbnail = data.data.results[0].thumbnail[0].path + data.data.results[0].thumbnail[0].extension;
+
+    var appendBlock = 
+        `
+        <div class="card" style="width: 300px;">
+            <div id="heroname" class="card-divider">${heroname}</div>
+            <img id="heroThumbnail" src=${thumbnail}>
+            <div class="card-section">
+                <h4 id="realName"></h4>
+                <p id="heroDescription">${heroDescription}</p>
+                <div id="embeddedVideo" class="responsive-embed">
+                    <iframe width="420" height="315" 
+                        src="https://www.youtube.com/embed/PAWjr7qo6Ds" 
+                        frameborder="0" allowfullscreen>
+                    </iframe>
+                </div>
+            </div>
+        </div>`;
+
+    for (var i=0; i<heroActualNames.length; i++) {
+        if (heroname === heroActualNames[i].heroname) {
+            $("#realName").text() = heroActualNames[i].realname;
+        }
     }
+
+    $("#recommended-hero").append(appendBlock);
+
+}
+
 
 //searchHero(searchword);
 $("#submit-button").on("click", function (event) {
@@ -49,6 +126,8 @@ $("#submit-button").on("click", function (event) {
 
     searchHero(searchword);
 })
+
+
 
 
 
