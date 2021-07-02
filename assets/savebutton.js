25700246e378cf62e3fd93130d2b8cd3c112b5db
Israel Magallon <browncans@gmail.com>
2021-07-01 18:24:21 -0700

    commenting out not working function

--- assets/savebutton.js
+++ assets/savebutton.js
@@ -1,19 +1,19 @@
-var heart = document.querySelector('#heart'); 
+// var heart = document.querySelector('#heart'); 
 var recentSearchList = [];
 
 
-heart.addEventListener('click', function() {
-    var searchword = $("#hero-input").val().trim();
-    storeHero(searchword);
-    addToHeartedList(searchword);
-})
-function addToHeartedList(){
-    var heartedHero = document.createElement('<a>');
-    heartedHero.classList.add("button expanded", "btn");
-    heartedHero.setAttribute("data-hero", searchword);
-    heartedHero.textContent = searchword;
-    heartContainer.appendChild(searchword)
-} 
+// heart.addEventListener('click', function() {
+//     var searchword = $("#hero-input").val().trim();
+//     storeHero(searchword);
+//     addToHeartedList(searchword);
+// })
+// function addToHeartedList(){
+//     var heartedHero = document.createElement('<a>');
+//     heartedHero.classList.add("button expanded", "btn");
+//     heartedHero.setAttribute("data-hero", searchword);
+//     heartedHero.textContent = searchword;
+//     heartContainer.appendChild(searchword)
+// } 
 
 function storeRecentSearch () {
     recentSearchList = JSON.parse(localStorage.getItem("recentlySearched")) || [];
